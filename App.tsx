85ba8391d31fed327ff4c16e995ffef8b3743b63
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2023-03-13 14:40:08 +0100

    build(item): Init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { NativeBaseProvider, Card } from "native-base";
+import { NativeBaseProvider, FlatList, Card, Center, Spinner } from "native-base";
+import { item } from "./src/components/item.tsx"
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products?limit=5')
       .then(res=>res.json())
-      .then(json=>setProducts(json));
+      .then(json=> {
+        setProducts(json)
+        setLoading(false)
+      });
   }, []);
 
   return (
     <NativeBaseProvider>
-      {products.map((product) => (
-        <Card key={product.id}>{product.title}</Card>
-      ))}
+      {loading ? (
+        <Center>
+          <Spinner />
+        </Center>
+      ) :
+      (
+        <FlatList
+	  data={products}
+	  renderItem={item}
+	  keyExtractor={(item) => {item.id.toString()}}
+	/>
+      )}
     </NativeBaseProvider>
   );
 }
---
+++ src/components/item.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Card } from "native-base";
+
+export const item = ({ item }) => {
+	return(
+		<Card>Hello, World!</Card>
+	);
+}
+
