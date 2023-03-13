55e6059e50b5c0768466783449cc21aa4772b0d3
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2023-03-13 14:00:16 +0100

    feat(App.tsx): Fetch API init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import React from "react";
-import { NativeBaseProvider, Box } from "native-base";
+import React, { useState, useEffect } from "react";
+import { NativeBaseProvider, Card } from "native-base";
 
-export default function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products?limit=5')
+      .then(res=>res.json())
+      .then(json=>setProducts(json));
+  }, []);
+
   return (
     <NativeBaseProvider>
-      <Box>Hello world</Box>
+      {products.map((product) => (
+        <Card key={product.id}>{product.title}</Card>
+      ))}
     </NativeBaseProvider>
   );
 }
+
+export default App;
