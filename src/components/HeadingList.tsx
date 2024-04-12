9a958810286bd0fc54861b1c5a6b516d50647d1c
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2024-04-13 01:27:16 +0200

    build: update lock files + run linters

--- src/components/HeadingList.tsx
+++ src/components/HeadingList.tsx
@@ -1,42 +1,36 @@
-import React from "react";
-import { 
-	View,
-	Heading, 
-	Box,
-	Text,
-	Select
-} from "native-base";
+import React from 'react';
+import {View, Heading, Box, Text, Select} from 'native-base';
 
 // Type definition for TypeScript
-type Props = {
+type HeadingProps = {
   category: string;
   onCategoryChange: (value: string) => void;
-  setLoading: (value: boolean) => void;
 };
 
-const HeadingList: React.FC = ({ category, onCategoryChange, setLoading }: Props) => {
+function HeadingList({category, onCategoryChange}: HeadingProps) {
   return (
     <View>
       <Heading fontSize="2xl" p="10" pb="5" textAlign="center">
         Our Products
       </Heading>
       <Box flexDirection="row" alignItems="center" mx="5" my="2">
-        <Text mr="5" width="auto" fontSize="xl">Categories:</Text>
+        <Text mr="5" width="auto" fontSize="xl">
+          Categories:
+        </Text>
         <Select
-        	flex="1"
+          flex="1"
           placeholder="All"
           selectedValue={category}
-          onValueChange={(value: string) => onCategoryChange(value)}
-        >
-        	<Select.Item label="All" value={"All"} />
-          <Select.Item label="Electronics" value={"electronics"} />
-          <Select.Item label="Jewelery" value={"jewelery"} />
+          onValueChange={(value: string) => onCategoryChange(value)}>
+          <Select.Item label="All" value={'All'} />
+          <Select.Item label="Electronics" value={'electronics'} />
+          <Select.Item label="Jewelery" value={'jewelery'} />
           <Select.Item label="Men's clothing" value={"men's clothing"} />
           <Select.Item label="Women's clothing" value={"women's clothing"} />
         </Select>
       </Box>
     </View>
   );
-};
+}
 
 export default HeadingList;
